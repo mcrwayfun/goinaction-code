f45218d21b0b21d705458898b3ffaca929497758
gavin.ma <gavin.ma@klook.com>
2019-12-29 23:12:09 +0800

    chapter2/search

---
+++ src/chapter2/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"chapter2/search"
+	"log"
+	"os"
+)
+
+// init在main之前调用
+func init() {
+	// 将日志输出到标准输出
+	log.SetOutput(os.Stdout)
+}
+
+// main 是整个程序的入口
+func main() {
+	// 使用特定的项做搜索
+	search.Run("president")
+}
